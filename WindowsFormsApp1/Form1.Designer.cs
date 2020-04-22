d5a799f219f0ca2a8abfeac5b2ad21baff487c4d
yoojinhee <52348335+yhujinhui@users.noreply.github.com>
2020-04-22 10:23:44 +0900

    first commit

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -66,6 +66,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.Opacity = 0.8D;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
